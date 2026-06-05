3bdb24575780e07a225f1b674a842c29dbd76df4
pixelgamesite-maker <pixelgamesite@gmail.com>
2026-06-05 11:05:52 +0100

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,7 @@
-import { Switch, Route, useLocation } from 'wouter';
-import Layout from './components/Layout';
 import Home from './pages/Home';
-import FAQ from './pages/FAQ';
-import About from './pages/About';
 
 function App() {
-  const [location] = useLocation();
-
-  return (
-    <Layout currentPath={location}>
-      <Switch>
-        <Route path="/" component={Home} />
-        <Route path="/faq" component={FAQ} />
-        <Route path="/about" component={About} />
-        <Route>
-          {() => { window.location.href = '/'; return null; }}
-        </Route>
-      </Switch>
-    </Layout>
-  );
+  return <Home />;
 }
 
 export default App;
